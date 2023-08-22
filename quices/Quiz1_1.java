694ee7cd5ad13e5358433304912b785b7e8b7fa9
SantiagoChaparroCambar <141367390+SantiagoChaparroCambar@users.noreply.github.com>
2023-08-21 21:52:28 -0500

    Update Quiz1_1.java

--- quices/Quiz1_1.java
+++ quices/Quiz1_1.java
@@ -1,3 +1,23 @@
-class Quiz1_1{
-  
+import java.util.Scanner;
+
+public class Quiz1_1{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+       // System.out.println("Ingrese un numero n: ");
+        int n = scanner.nextInt();
+        System.out.flush();
+        for(int i=1; i<=n;i++){
+            if(i%3 == 0 && i%5 ==0){
+              System.out.println("fizzbuzz");
+            }else if(i%3 == 0){
+               System.out. println("fizz");
+            }else if(i%5==0){
+                System.out.println("buzz");
+            } else  if(i<n){
+            System.out.println(i);
+            }
+        }
+    
+    }
 }
+
