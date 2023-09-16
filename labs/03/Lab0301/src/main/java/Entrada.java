03a49cb5352d1a8d98f0c04ad031d5dfa386fb3f
SantiagoChaparroCambar <141367390+SantiagoChaparroCambar@users.noreply.github.com>
2023-09-16 12:33:05 -0500

    Update Entrada.java
    ayuda 02 del profe

--- labs/03/Lab0301/src/main/java/Entrada.java
+++ labs/03/Lab0301/src/main/java/Entrada.java
@@ -1,21 +1,52 @@
 
 import java.util.Scanner;
-
-
+import java.util.LinkedList;
+import java.util.Arrays;
+import java.io.File;
 
 public class Entrada {
     private static Scanner input = new Scanner(System.in);
     
            
     public static int readInt(String mss){
-        System.out.println(mss);
-        return input.nextInt();
-   
+       String str = null;
+        do {
+            str = readText(mss);
+        } while (!str.matches("-?\\d+(\\.\\d+)?"));
+        return Integer.parseInt(str);
     }
+
+    public static int readInt(String mss, int min, int max) {
+        int val = 0;
+        do {
+            val = readInt(mss);
+        } while (val < min || val > max);
+        return val;
+    }
+    
     public static String readText(String mss){
         System.out.println(mss);
         return input.nextLine();
-   
     }
-            
+public static LinkedList<Asignatura> loadFile(String filename) {
+        LinkedList<Asignatura> listAsignatura = new LinkedList<>();
+        Scanner file = null;
+        try {
+            file = new Scanner(new File(filename));
+            while (file.hasNextLine()) {
+                String line[] = file.nextLine().split(";");
+                System.out.println(Arrays.toString(line));
+                listAsignatura.add(new Asignatura(Integer.parseInt(line[0]),
+                    line[1], Integer.parseInt(line[2]), Integer.parseInt(line[3]),
+                    line[4]));
+            }
+            System.out.println();
+            file.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return listAsignatura;
+    }
+    
 }
