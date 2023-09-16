3b0eb5e05e8655dc54cfc1a9a39292129f41fadf
SantiagoChaparroCambar <141367390+SantiagoChaparroCambar@users.noreply.github.com>
2023-09-16 12:29:19 -0500

    Update Lab0301.java
    ayuda 02 del profe

--- labs/03/Lab0301/src/main/java/Lab0301.java
+++ labs/03/Lab0301/src/main/java/Lab0301.java
@@ -11,27 +11,56 @@ import java.util.Queue;
  * @author Estudiante
  */
 public class Lab0301 {
+    static LinkedList<Asignatura> materias = new LinkedList<>();
 static Queue<Registro> regis =new LinkedList<>();
+    static Queue<String> codes = new LinkedList<>();   // Cola de códigos de estudiantes
 
     public static void main(String[] args) {
-        
-        registro_estudiante();
-        registro_asignatura();
-        reporte_registro_final();
+        boolean siga = true;
+       carga_asignaturas(args[0]);
+        do {
+            siga = registro_estudiante();
+            if (!siga) break;
+            registro_asignaturas();
+            reporte_registro();
+        } while (true);
         
     }
+    public static void carga_asignaturas(String archivo) {
+        materias = Entrada.loadFile(archivo);
+    }
+    
     public static void registro_estudiante(){
-        
-        String codigo= Entrada.readText("Codigo estudiante");
-        String nombre = Entrada.readText("Nombre estudiante");
-        String correo = Entrada.readText("correo estudiante");
-        int semestre = Entrada.readInt("semestre  estudiante");
-        Estudiante estud = new Estudiante(codigo,nombre,correo,semestre);
-    regis.add(new Registro(estud));
-        
+String codigo = Entrada.readText("DATOS ESTUDIANTE\n\n Código (-1=FIN): ");
+        if (codigo.equals("-1")) return false;
+        if (codes.contains(codigo)) {
+            System.out.println(" Codigo " + codigo + " ya registrado\n");
+            return true;
+        }
+        String nombre = Entrada.readText(" Nombre: ");
+        String correo = Entrada.readText(" Correo: ");   
+        int semestre = Entrada.readInt(" Semestre: ");
+        System.out.println();
+
+        Estudiante estud = new Estudiante(codigo, nombre, correo, semestre);
+
+        regis.add(new Registro(estud));
+        codes.add(codigo);
+
+        return true;   
     }
     public static void registro_asignatura(){
-        
+        do {
+            int sem = Entrada.readInt("\nVer asignaturas del semestre: ", 1, 10);
+            mostrar_asignaturas(sem);
+        } while (false);
+    }
+     public static void mostrar_asignaturas(int sem) {
+        for (Asignatura s : materias) {
+            if (s.getSemestre() == sem)
+                System.out.println(s);
+        }
+        System.out.println();
     }
     public static void reporte_registro_final(){
         
