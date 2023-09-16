5d842b369b4bac0c714d5c2c0949fe2e1a4e0dad
SantiagoChaparroCambar <141367390+SantiagoChaparroCambar@users.noreply.github.com>
2023-09-16 12:30:34 -0500

    Update Asignatura.java
    ayuda 02 del profe

--- labs/03/Lab0301/src/main/java/Asignatura.java
+++ labs/03/Lab0301/src/main/java/Asignatura.java
@@ -1,9 +1,26 @@
 
 public class Asignatura {
-    int codAsignatura;
+    int codigo;
     String nombre;
     int semestre;
     int num_creditos;
     String horario;
     
+     Asignatura(int codigo, String nombre, int semestre, int num_creditos,
+            String horario) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.semestre = semestre;
+        this.num_creditos = num_creditos;
+        this.horario = horario;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-7d  %-35s%5d%7s", codigo, nombre, num_creditos, horario);
+    }
 }
