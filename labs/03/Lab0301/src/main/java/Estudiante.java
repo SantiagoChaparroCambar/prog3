35f48209230377d56c1d416ab19b10cb70e5dd33
SantiagoChaparroCambar <141367390+SantiagoChaparroCambar@users.noreply.github.com>
2023-09-16 12:34:36 -0500

    Update Estudiante.java
    ayuda 02 profe

--- labs/03/Lab0301/src/main/java/Estudiante.java
+++ labs/03/Lab0301/src/main/java/Estudiante.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Estudiante
- */
 public class Estudiante {
     private String codigo;
     private String nombre;
