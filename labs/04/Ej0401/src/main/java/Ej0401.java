f81cb50072f5b91100aa63c8fcdbfbf5d8a184df
SantiagoChaparroCambar <141367390+SantiagoChaparroCambar@users.noreply.github.com>
2023-09-28 14:53:02 -0500

    Add files via upload

---
+++ labs/04/Ej0401/src/main/java/Ej0401.java
@@ -0,0 +1,21 @@
+
+import java.util.Timer;
+import java.util.TimerTask;
+public class Ej0401 {
+ Timer timer;
+ public Ej0401(int segundos) {
+ timer = new Timer();
+ timer.schedule(new Recordatorio(),segundos*1000);//Programa la tarea
+ }
+ class Recordatorio extends TimerTask {
+ @Override
+ public void run() {
+ System.out.println("Tiene un recordatorio!");
+ timer.cancel(); //Termina el hilo del timer
+ }
+ }
+ public static void main(String args[]) {
+ System.out.println("Inicio del programa");
+ new Ej0401(5);
+ }
+}
