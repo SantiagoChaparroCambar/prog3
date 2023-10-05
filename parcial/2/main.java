5ee128c5628f365efddf52500700b912761303f9
SantiagoChaparroCambar <141367390+SantiagoChaparroCambar@users.noreply.github.com>
2023-10-05 14:22:30 -0500

    Update and rename main.txt to main.java
    menu de usuario

---
+++ parcial/2/main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Timer;
+
+public class main {
+public static void main(String[] args){
+
+  Scanner scanner = new Scanner(System.in);
+  Timer timer = new Timer();
+
+  while(true){
+    System.out.println(" 1. Crear Temporizador ");
+    System.out.println(" 2. Detener Temporizador ");
+    System.out.println(" 3. Reiniciar Temporizador ");
+    System.out.println(" 4. Salir ");
+    System.out.println(" Seleccione una opcion: ");
+    
+
+  }
+}
+  
+}
