c41e0f1048afb7a41d7b278d45767421db8e52f2
SantiagoChaparroCambar <141367390+SantiagoChaparroCambar@users.noreply.github.com>
2023-10-07 10:14:23 -0500

    Update Temporizador.java

--- parcial/2/Temporizador.java
+++ parcial/2/Temporizador.java
@@ -1,13 +1,25 @@
 import java.util.TimerTask;
 
-public class Temporizador extends TimerTask{
-  private String mensaje;
+public class Temporizador extends TimerTask {
+    private String mensaje;
+    private long retraso; // Cambié int a long
 
-  public Temporizador(String mensaje){
-    this.mensaje= mensaje;
-  }
-  @Override
-  public void run(){
-    System.out.println(mensaje);
-  }
+    public Temporizador(String mensaje, long retraso) { // Agregué el constructor con mensaje y retraso
+        this.mensaje = mensaje;
+        this.retraso = retraso;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public long getRetraso() {
+        return retraso;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(mensaje);
+    }
 }
+
