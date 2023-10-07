f697ba1f295069d707d4d6a36d8ee5cca76400cf
SantiagoChaparroCambar <141367390+SantiagoChaparroCambar@users.noreply.github.com>
2023-10-07 10:13:18 -0500

    Update main.java
    ya corregi todos los errores del codigo, el problema fue que Temporizador no estaba en el mismo paquete del Main

--- parcial/2/main.java
+++ parcial/2/main.java
@@ -3,82 +3,81 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.HashMap;
 
-public class main {
-  static Timer timer = new Timer();
-  static HashMap<Integer, TimerTask> temporizadores = new HashMap<>();
+public class Main {
   
-public static void main(String[] args){
-  Scanner scanner = new Scanner(System.in);
-  
+    static Timer timer = new Timer();
+    static HashMap<Integer, TimerTask> temporizadores = new HashMap<>();
 
-  while(true){
-    System.out.println(" 1. Crear Temporizador ");
-    System.out.println(" 2. Detener Temporizador ");
-    System.out.println(" 3. Reiniciar Temporizador ");
-    System.out.println(" 4. Salir ");
-    System.out.println(" Seleccione una opcion: ");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    int opc = scanner.nextInt();
-    scanner.nextLine();
+        while (true) {
+            System.out.println("1. Crear Temporizador ");
+            System.out.println("2. Detener Temporizador ");
+            System.out.println("3. Reiniciar Temporizador ");
+            System.out.println("4. Salir ");
+            System.out.println("Seleccione una opción: ");
 
-    switch(opc){
-      case 1: // crear el temporizador 
-      System.out.println(" Ingrese el mensaje del temporizador: ");
-        String mensaje = scanner.nextLine();
-      System.out.println(" Ingrese el tiempo de espera en segundos : ");
-        int segundos = scanner.nextInt();
-        scanner.nextLine();
-        
-        // crear una instancia de Temporizador y programar su ejecucion
-        Temporizador temporizador = new Temporizador(mensaje);
-        temporizadores.put(temporizadores.size() + 1, temporizador);// asignar un ID a el Temporizador 
-        timer.schule(temporizador,segundos*1000); // de milisegundos a segundos
-        break;
+            int opc = scanner.nextInt();
+            scanner.nextLine();
 
-      case 2:// detener Temporizador 
-        System.out.println("Ingrese el ID del temporizador a detener: ");
-        int idDetener = scanner.nextInt();
-        scanner.nextLine();
+            switch (opc) {
+                case 1: // crear temporizador 
+                    System.out.println("Ingrese el mensaje del temporizador: ");
+                    String mensaje = scanner.nextLine();
+                    System.out.println("Ingrese el tiempo de espera en segundos: ");
+                    long segundos = scanner.nextInt(); // Cambié a long
+                    scanner.nextLine();
 
-        TimerTask tareaDetener = temporizadores.get(idDetener);
-        if(tareaDetener != null){
-          tareaDetener.cancel();// cancelar la tarea del temporizador 
-          temporizadores.remove(idDetener);//eliminaar temporizador de la lista
-          System.out.println(" Temporizador Detenido !!!!!!!");
-        } else {
-          System.out.println(" Temporizador no Encontrado !!!!!!");
-        }
-        break;
-case 3: // reiniciar temporizador 
-                   
-                    System.out.print("Ingrese el ID del temporizador que desea reiniciar: ");
+                    //// crear una instancia de Temporizador y programar su ejecucion
+                    Temporizador temporizador = new Temporizador(mensaje, segundos); // Pasé mensaje y segundos
+                    temporizadores.put(temporizadores.size() + 1, temporizador);// asignar un ID a el Temporizador
+                    timer.schedule(temporizador, segundos * 1000);// de milisegundos a segundos
+                    break;
+
+                case 2:// detener Temporizador 
+                    System.out.println("Ingrese el ID del temporizador a detener: ");
+                    int idDetener = scanner.nextInt();
+                    scanner.nextLine();
+
+                    TimerTask tareaDetener = temporizadores.get(idDetener);
+                    if (tareaDetener != null) {
+                        tareaDetener.cancel();// cancelar la tarea del temporizador 
+                        temporizadores.remove(idDetener);//eliminaar temporizador de la lista
+                        System.out.println("Temporizador Detenido!");
+                    } else {
+                        System.out.println("Temporizador no Encontrado!");
+                    }
+                    break;
+
+                case 3:// reiniciar temporizador
+                    System.out.println("Ingrese el ID del temporizador que desea reiniciar: ");
                     int idReiniciar = scanner.nextInt();
                     scanner.nextLine();
 
                     TimerTask tareaReiniciar = temporizadores.get(idReiniciar);
                     if (tareaReiniciar != null) {
                         tareaReiniciar.cancel(); // Cancelar la tarea del temporizador
-                        temporizadores.remove(idReiniciar); // Eliminar el temporizador de la lista
+                        temporizadores.remove(idReiniciar);// Eliminar el temporizador de la lista
 
                         // Volver a programar la tarea
-                        Temporizador nuevoTemporizador = new Temporizador(((Temporizador) tareaReiniciar).getMensaje());
+                        Temporizador nuevoTemporizador = new Temporizador(((Temporizador) tareaReiniciar).getMensaje(), ((Temporizador) tareaReiniciar).getRetraso());
                         temporizadores.put(idReiniciar, nuevoTemporizador);
-                        timer.schedule(nuevoTemporizador, ((Temporizador) tareaReiniciar).getRetraso());
+                        timer.schedule(nuevoTemporizador, nuevoTemporizador.getRetraso());
 
                         System.out.println("Temporizador reiniciado.");
                     } else {
                         System.out.println("Temporizador no encontrado.");
                     }
                     break;
+
                 case 4:
                     timer.cancel();
                     System.exit(0);
                 default:
                     System.out.println("Opción no válida. Intente de nuevo.");
                     break;
-
+            }
+        }
     }
-  }
-}
-  
 }
