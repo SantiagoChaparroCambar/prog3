8360f704dc2de93e2822076f12abe806498498cc
SantiagoChaparroCambar <141367390+SantiagoChaparroCambar@users.noreply.github.com>
2023-10-26 15:02:42 -0500

    Add files via upload

---
+++ labs/05/Lab05/src/Tramo.java
@@ -0,0 +1,12 @@
+
+public class Tramo {
+    int fallecidos;
+    String tramos;
+    String Departamento; 
+    
+    public Tramo(int fallecidos, String tramos, String Departamento) {
+        this.fallecidos = fallecidos;
+        this.tramos = tramos;
+        this.Departamento = Departamento;
+    }
+}
